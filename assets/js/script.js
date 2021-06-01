2ab7e6250421e5d6e94bb0d91339c178a2d33ef1
kotonitta <rosanna.espinoza@gmail.com>
2021-05-31 21:24:48 -0400

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var manzanas_raul=4
+var manzanas_pedro=2
+var peras_alberto=8
+var frutillas_pedro=4
+
+
+var sumando1 = manzanas_pedro;
+var sumando2 = manzanas_raul;
+
+var total_suma = sumando1 + sumando2;
+
+document.write("<h4>total de manzanas</h4>",total_suma);
+
+var dividendo = frutillas_pedro
+var divisor = 2
+
+total_divi = dividendo / divisor ;
+
+document.write("<h4>Frutillas para 2 personas</h4>",total_divi);
+
+var sumando1 = manzanas_pedro;
+var sumando2 = manzanas_raul;
+var sumando3 = peras_alberto;
+var sumando4 = frutillas_pedro;
+
+var total_suma2 = sumando1 + sumando2 + sumando3 + sumando4;
+
+document.write("<h4>Total de frutas</h4>",total_suma2);
